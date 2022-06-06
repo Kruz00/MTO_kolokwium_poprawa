76edbcb9c7d4979125e7a122632b7d2340e8592c
Hubert Krauza <hubertkrauza@gmail.com>
2022-06-06 17:37:59 +0200

    Tests setUp

--- testsReactor/src/test/java/edu/iis/mto/oven/OvenTest.java
+++ testsReactor/src/test/java/edu/iis/mto/oven/OvenTest.java
@@ -2,17 +2,42 @@ package edu.iis.mto.oven;
 
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.equalTo;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
 @ExtendWith(MockitoExtension.class)
 class OvenTest {
 
+    Oven oven;
+    BakingProgram bakingProgram;
+    BakingResult bakingResult;
+
+    @Mock
+    HeatingModule heatingModule;
+
+    @Mock
+    Fan fan;
+
+    @BeforeEach
+    void setUp() {
+        oven = new Oven(heatingModule, fan);
+        bakingProgram = BakingProgram.builder().withCoolAtFinish(false).withInitialTemp(0).build();
+    }
+
     @Test
     void itCompiles() {
         assertThat(true, equalTo(true));
+    }
+
+    @Test
+    void shouldWorks() {
+        bakingResult = oven.runProgram(bakingProgram);
+        assertTrue(bakingResult.isSuccess());
     }
 
 }
